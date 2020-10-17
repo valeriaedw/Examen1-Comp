9b2c499d41ed5966b91aed37812b2d44da6fccde
valeriaedw <valeriaedwards17@gmail.com>
2020-10-16 19:31:03 -0600

    Examen primer commit

---
+++ Mastermind/src/mastermind/Mastermind.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package mastermind;
+
+/**
+ *
+ * @author Valeria
+ */
+public class Mastermind {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        Game myGame = new Game();
+        myGame.setVisible(true);
+
+    }
+    
+}
